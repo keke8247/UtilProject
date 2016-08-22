c8d7cb9f702074983f28cf74bbf8262d76fa71bd
wdk <wdk@erongdu.com>
2016-08-22 16:20:22 +0800

    测试线程

---
+++ src/main/java/com/wdk/erongdu/common/test/thread/Foo.java
@@ -0,0 +1,16 @@
+package com.wdk.erongdu.common.test.thread;
+
+public class Foo {
+	private int x = 100;
+	public int getX(){
+		return x;
+	}
+	
+	public int fix(int y){
+		x = x-y;
+		return x;
+	}
+	public static void main(String[] args) {
+		System.out.println("~~~");
+	}
+}
