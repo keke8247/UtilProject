5c7876a6b7cce9cec6d76c77d2088e21148408d1
wdk <wdk@erongdu.com>
2016-11-14 11:35:13 +0800

    测试

---
+++ src/main/java/com/erongdu/test/util/SystemCacheTest.java
@@ -0,0 +1,15 @@
+package com.erongdu.test.util;
+
+
+/**
+ *	@Description
+ *	通过CacheSize api 访问CPU的缓存信息
+ *  @author wangdk,wangdk@erongdu.com
+ *  @CreatTime 2016年11月11日 下午4:30:10
+ *  @since version 1.0.0
+ */
+public class SystemCacheTest {
+	public static void main(String[] args) {
+		
+	}
+}
